690f97c3d4871d2b14e298344a02c91cf6e9d7f6
Shubham-791 <shubhamv791@gmail.com>
2020-01-15 19:43:55 +0530

    ngTemplateOutlet and ngTemple Demo Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  links = [{type:'simple',name:'linkSimple'},{type:'complex',name:'linkComplex'}];
 }
